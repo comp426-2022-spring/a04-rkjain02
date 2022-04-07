38e51848932fd2c99154d5eebf0918225c51fae8
rishab <rkjain02@ad.unc.edu>
2022-04-07 13:36:30 -0400

    fix log argument

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const fs = require("fs");
 const morgan = require('morgan');
 
 const args = require("minimist")(process.argv.slice(2))
+console.log(args)
 args['port']
+args['log']
 const port = args.port || 5555;
+const log = args.log || false;
 
 //console.log(args["debug"]) 
 
@@ -38,7 +41,7 @@ if(args['debug']) {
     });
 
 }
-if (args['log']) {
+if (log) {
     const WRITESTREAM = fs.createWriteStream(__dirname+ '/access.log', { flags: 'a' })
     const stmt = db.prepare('SELECT * FROM accesslog').all()
     // Set up the access logging middleware
