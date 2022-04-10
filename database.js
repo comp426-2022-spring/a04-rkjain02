d52237101ccbed37ff5f85d64e7b17f2d512ae25
rishab <rkjain02@ad.unc.edu>
2022-04-10 15:59:19 -0400

    database type fixed

--- database.js
+++ database.js
@@ -18,7 +18,19 @@ if (row === undefined) {
     console.log('Your database appears to be empty. I will initialize it now.');
     // Set a const that will contain your SQL commands to initialize the database.
     const sqlInit = `
-        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time TEXT, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, secure TEXT, status TEXT, referer TEXT, useragent TEXT  );
+        CREATE TABLE accesslog ( 
+            id INTEGER PRIMARY KEY,
+            remoteaddr VARCHAR, 
+            remoteuser VARCHAR, 
+            time VARCHAR, 
+            method VARCHAR, 
+            url VARCHAR, 
+            protocol VARCHAR, 
+            httpversion NUMERIC, 
+            secure VARCHAR, 
+            status INTEGER, 
+            referer VARCHAR, 
+            useragent VARCHAR  );
     `;
     // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
