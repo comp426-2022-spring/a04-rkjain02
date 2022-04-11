547bd153ed6ca27e4a141986ef0eb1619d73541e
rishab <rkjain02@ad.unc.edu>
2022-04-11 13:21:12 -0400

    help log

--- server.js
+++ server.js
@@ -14,26 +14,22 @@ const log = (args.log) == ('true')
 
 //console.log(args["debug"]) ß
 
-const help = (`
-server.js [options]
+const help = (`server.js [options]
+--port	Set the port number for the server to listen on.Must be an integer
+              between 1 and 65535.
+--debug	If set to \`true\`, creates endlpoints /app/log/access/ which returns
+              a JSON access log from the database and /app/error which throws 
+              an error with the message "Error test successful." Defaults to 
+              \`false\`.
+  --log		If set to false, no log files are written. Defaults to true.
+              Logs are always written to database.
+  --help	Return this message and exit.`)
 
---port	Set the port number for the server to listen on. Must be an integer
-            between 1 and 65535.
 
---debug	If set to true, creates endlpoints /app/log/access/ which returns
-            a JSON access log from the database and /app/error which throws 
-            an error with the message "Error test successful." Defaults to 
-            false.
-
---log		If set to false, no log files are written. Defaults to true.
-            Logs are always written to database.
-
---help	Return this message and exit.
-`)
 
 if (args["help"]) {
     console.log(help)
-    process.exit(0) 
+    process.exit(0)  
 }
 
 
