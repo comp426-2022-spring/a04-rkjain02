1f820a4956482d9dd8f6d11479d2e82b269cbf49
rishab <rkjain02@ad.unc.edu>
2022-04-12 13:07:33 -0400

    help message

--- server.js
+++ server.js
@@ -14,13 +14,11 @@ const log = (args.log) == ('true')
 
 //console.log(args["debug"]) ß
 
-const help = (`
-server.js [options]
-
+const help = (`server.js [options]
 --port	Set the port number for the server to listen on. Must be an integer
             between 1 and 65535.
 
---debug	If set to true, creates endlpoints /app/log/access/ which returns
+--debug	    If set to true, creates endlpoints /app/log/access/ which returns
             a JSON access log from the database and /app/error which throws 
             an error with the message "Error test successful." Defaults to 
             false.
@@ -31,10 +29,35 @@ server.js [options]
 --help	Return this message and exit.
 `)
 
+console.log(`server.js [options]
+  --port	    Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+  --debug	    If set to \`true\`, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            \`false\`.
+
+  --log		    If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+  --help	    Return this message and exit.`)
 
 
 if (args["help"]) {
-    console.log(help)
+    console.log(`server.js [options]
+    --port	      Set the port number for the server to listen on. Must be an integer
+                between 1 and 65535.
+
+    --debug	      If set to \`true\`, creates endlpoints /app/log/access/ which returns
+                a JSON access log from the database and /app/error which throws 
+                an error with the message "Error test successful." Defaults to 
+                \`false\`.
+
+    --log         If set to false, no log files are written. Defaults to true.
+                Logs are always written to database.
+
+    --help	      Return this message and exit.`)
     process.exit(0)  
 }
 
